d88b23d1e295f37695b2153857ac3e25375609e3
Peter Lewis <peter.ka.lewis@gmail.com>
2025-08-19 12:50:38 -0400

    Initial commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/register', methods=['GET', 'POST'])
+    def user_info():
+        if request.method == 'POST':
+            first_name = request.form.get('first-name')
+            last_name = request.form.get('last-name')
+            email = request.form.get('email')
+            pswd = request.form.get('pswd')
+            pswd_repeat = request.form.get('pswd-repeat')
+
+            print(first_name, last_name, email, pswd, pswd_repeat)
+            #return f'{first_name}, {last_name}, {email} {pswd}, {pswd_repeat}'
+            return f'Info received'
+        else:
+            return render_template('register-form.html')
+    return app
