17f54e950f00f8986b4c34c7dd38c965b4a52dcc
Peter Lewis <peter.ka.lewis@gmail.com>
2025-10-22 12:11:01 -0400

    add function for automated form input

--- tests/test_register.py
+++ tests/test_register.py
@@ -14,44 +14,46 @@ def driver():
     yield driver    
     driver.quit()
 
-def test_register_form(driver):
+def fill_form(driver, username, email, password, confirm_password):
     driver.get("http://localhost:5000/")
-    
     title = driver.title
     assert "Registration Page" in title
 
     username_box = driver.find_element(By.NAME, "username")
-    username_box.send_keys("Jill_Brown")
+    username_box.send_keys(username)
     username_value = username_box.get_attribute("value")    
     time.sleep(1)
-    assert "Jill_Brown" in username_value
+    assert username in username_value
 
     email_box = driver.find_element(By.NAME, "email")
-    email_box.send_keys("jill.brown@gmail.com")
+    email_box.send_keys(email)
     email_value = email_box.get_attribute("value")
     time.sleep(1)
-    assert "jill.brown@gmail.com" in email_value
+    assert email in email_value
 
     password_box = driver.find_element(By.NAME, "password")
-    password_box.send_keys("Zxcvbnm1")
+    password_box.send_keys(password)
     password_value = password_box.get_attribute("value")
     time.sleep(1)
-    assert "Zxcvbnm1" in password_value
+    assert password in password_value
 
     confirm_password_box = driver.find_element(By.NAME, "confirm_password")
-    confirm_password_box.send_keys("Zxcvbnm1")
-    confirm_password_value = password_box.get_attribute("value")
+    confirm_password_box.send_keys(confirm_password)
+    confirm_password_value = confirm_password_box.get_attribute("value")
     time.sleep(1)
-    assert "Zxcvbnm1" in confirm_password_value
+    assert confirm_password in confirm_password_value
 
-    register_btn = driver.find_element(By.NAME, "submit")
-    register_btn.click()
-    time.sleep(1)
-    assert "Registration successful!" in driver.page_source, "Expected successful message not found"
-    
-    
+    driver.find_element(By.NAME, "submit").click()
+
     print("\nPage title is:", title)
     print("Username input:", username_value)
     print("Email input:", email_value)
     print("Password input:", password_value)
     print("Password input:", confirm_password_value)
+
+def test_form_valid(driver):
+    fill_form(driver, "Jill_Brown", "jill.brown@gmail.com", "Zxcvbnm1", "Zxcvbnm1")
+    assert "Registration successful!" in driver.page_source, "Expected successful message not found"
+
+def test_form_invalid(driver):
+    fill_form(driver, ".", "jill.brown@@gmail.com", "badpsw", "Zxcvbnm1")
