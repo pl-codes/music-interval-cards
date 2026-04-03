f68bc51c92ab737b45b14fc2e06fed5f79b9bb35
Peter Lewis <peter.ka.lewis@gmail.com>
2026-04-02 22:23:53 -0400

    Add login test automation for valid input

---
+++ tests/test_login.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import pytest
+
+@pytest.fixture
+def driver():
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+    driver.maximize_window()
+    driver.implicitly_wait(3)
+    yield driver        
+    driver.quit()
+
+def fill_form(driver, email, password):
+    driver.get("http://localhost:5000/login")
+    time.sleep(1)
+    title = driver.title
+    assert "Login Page" in title   
+
+    email_box = driver.find_element(By.NAME, "email")
+    email_box.send_keys(email)
+    email_value = email_box.get_attribute("value")
+    time.sleep(1)
+    assert email in email_value
+
+    password_box = driver.find_element(By.NAME, "password")
+    password_box.send_keys(password)
+    password_value = password_box.get_attribute("value")
+    time.sleep(1)
+    assert password in password_value
+
+    print("\nPage title is:", title)    
+    print("Email input:", email_value)
+    print("Password input:", password_value)
+
+def test_form_valid(driver):
+    fill_form(driver, "jill.brown@gmail.com", "Zxcvbnm1")
+    time.sleep(1)
+    #assert "Registration successful!" in driver.page_source, "Expected successful message not found"
